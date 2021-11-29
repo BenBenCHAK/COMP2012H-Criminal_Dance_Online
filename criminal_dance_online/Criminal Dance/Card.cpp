c0600ce5a0045ca9889b1da32c007ab5c88d7bc3
Bosco <93004484+BossscoLeung@users.noreply.github.com>
2021-11-29 22:38:37 +0800

    Update Card.cpp

--- criminal_dance_online/Criminal Dance/Card.cpp
+++ criminal_dance_online/Criminal Dance/Card.cpp
@@ -1,13 +1,13 @@
 #include "Card.h"
 
 // constructor initialize the card type and set the card can be used
-Card::Card(int card_type) : type(static_cast<Type>(card_type)), useable(true) {
+Card::Card(int card_type) : type(static_cast<Type>(card_type)), selectable(true) {
 
 }
 
 // mutator that set whether the card can be used or not
-void Card::set_useable(bool k){
-    this->useable = k;
+void Card::set_selectable(bool k){
+    this->selectable = k;
 }
 
 // accessors
@@ -16,5 +16,5 @@ Card::Type Card::get_type() const{
 }
 
 bool Card::can_use() const{
-    return useable;
+    return selectable;
 }
