010dc360ced19d9c09f34156b95c12e82b25fd00
Bosco <93004484+BossscoLeung@users.noreply.github.com>
2021-11-30 00:03:14 +0800

    Update Player.cpp

--- criminal_dance_online/Criminal Dance/Player.cpp
+++ criminal_dance_online/Criminal Dance/Player.cpp
@@ -11,7 +11,8 @@ Player::~Player(){
     }
 }
 
-void Player::add_new_player(string name){ //Circularly linked
+
+void Player::add_new_player(string name){
     Player* new_player = new Player(name);
     new_player->first_player = false;
     new_player->prev_player = prev_player;
@@ -19,6 +20,10 @@ void Player::add_new_player(string name){ //Circularly linked
 
     prev_player->next_player = new_player;
     prev_player = new_player;
+}
+
+void Player::reset(){
+    first_player = false;
 }
 
 bool Player::is_first_player(){
@@ -30,18 +35,99 @@ bool Player::is_first_player(){
     return first_player;
 }
 
-bool Player::is_player1(){
-    return first_player;
-}
-
-void Player::reset(){
-    first_player = false;
-}
-
 void Player::init_hand(vector<int> card_list){
     for (int i = 0; i<card_list.size(); i++){
         hand.push_back(new Card(card_list[i]));
     }
+}
+
+
+string Player::get_name() const{
+    return name;
+}
+
+bool Player::is_player1() const{
+    return first_player;
+}
+
+string Player::get_hand() const{
+    string card_list = "[ ";
+    
+    for (int i = 0; i<hand.size(); i++){
+        switch(static_cast<int>(hand[i]->get_type())){
+            case 1:{
+                card_list += "FIRST_FINDER  ";
+                break;
+            }
+            case 2:{
+                card_list += "CULPRIT  ";
+                break;
+            }
+            case 3:{
+                card_list += "DETECTIVE  ";
+                break;
+            }
+            case 4:{
+                card_list += "ALIBI  ";
+                break;
+            }
+            case 5:{
+                card_list += "ACCOMPLICE  ";
+                break;
+            }
+            case 6:{
+                card_list += "WITNESS  ";
+                break;
+            }
+            case 7:{
+                card_list += "BYSTANDER  ";
+                break;
+            }
+            case 8:{
+                card_list += "BASTET  ";
+                break;
+            }
+            case 9:{
+                card_list += "INFO_EXCHANGE  ";
+                break;
+            }
+            case 10:{
+                card_list += "RUMOR  ";
+                break;
+            }
+            case 11:{
+                card_list += "BARTER  ";
+                break;
+            }
+            case 12:{
+                card_list += "JUVENILE  ";
+                break;
+            }
+        }
+    }
+    
+    card_list += "]";
+    return card_list;
+}
+
+string Player::get_type() const{
+    switch(type){
+        case Player::Type::CIVILIAN:
+            return "Civilian";
+        case Player::Type::CULPRIT:
+            return "Culprit";
+        case Player::Type::ACCOMPLICE:
+            return "Accomplice";
+    }
+    return "";
+}
+
+Card* Player::get_card(const int& index) const{
+    return hand[index];
+}
+
+void Player::set_type(const Player::Type& type){
+    this->type = type;
 }
 
 int Player::has_card(int type){
@@ -53,41 +139,6 @@ int Player::has_card(int type){
     return -1;
 }
 
-int Player::exchange_select_card(){
-    if(hand.size() == 0)
-        return -1;
-    if(hand.size() == 1 && hand[0]->can_use() == false)
-        return -1;
-    int selected_card;
-    cout << get_name() << ": Please select a card to pass/exchange/abandon." << endl;
-    cout << "Card List: " << get_hand() << endl;
-    do{
-        cin >> selected_card;
-        selected_card--;
-        if(hand[selected_card]->can_use() == false){
-            cout << "This card cannot be passed again." << endl;
-        }
-    }while(hand[selected_card]->can_use() == false);
-    return selected_card;
-}
-
-int Player::draw_select_card(){
-    if(prev_player->hand.size() == 0)
-        return -1;
-    if(prev_player->hand.size() == 1 && prev_player->hand[0]->can_use() == false)
-        return -1;
-    int draw_selected_card;
-    cout << get_name() << ": Please select a card you want to draw from " << prev_player->get_name() << endl;
-    cout << "Card List: " << prev_player->get_hand() << endl;
-    do{
-        cin >> draw_selected_card;
-        draw_selected_card--;
-        if(prev_player->hand[draw_selected_card]->can_use() == false){
-            cout << "This card cannot be drawn again." << endl << endl;
-        }
-    }while(prev_player->hand[draw_selected_card]->can_use() == false);
-    return draw_selected_card; 
-}
 
 int Player::select_card(){
     if(hand.size() == 0)
@@ -109,55 +160,35 @@ int Player::select_card(){
     return selected_card;
 }
 
-Player* Player::select_target(){
-    Player* target = this;
-    int player_index;
-    int target_index;
-    while(true){
-        target = this;
-        print_select_target_list(player_index);
-        cin >> target_index;
-        while (target_index<0 || target_index>player_index){
-            cout << "Out of Bounds." << endl;
-            print_select_target_list(player_index);
-            cin >> target_index;
-        }
-
-        for (int i=0; i<target_index; i++){
-            target = target->next_player;
-        }
-        if(target->hand.size() > 0) // if the target has no cards repeat to choose target again 
+// only for use card select but not the passing/exchanging/drawing/abandoning
+bool Player::can_select_card(int index){
+    if (index>=hand.size()||index<0){
+        return false;
+    }
+    switch (static_cast<int>(hand[index]->get_type())){
+        case 1: 
+            return true;
+        case 2: // cant use CULPRIT unless it is the less card
+            if (hand.size()!=1){
+                return false;
+            }
             break;
-        cout << "You can't choose players with no cards." << endl << endl;
+
+        case 3: // DETECTIVE cant be use when having 4 cards unless holding 1 CULPRIT and 3 DETECTIVE or 4 DETECTIVE
+            if (hand.size() == 4){
+                for(int i = 0; i < hand.size(); ++i){
+                    if (static_cast<int>(hand[i]->get_type()) != 3 && static_cast<int>(hand[i]->get_type()) != 2)
+                        return false;
+                }
+            }
+            break;
+
+        default: // force first finder to use first finder
+            if(has_card(1) != -1)
+                return false;
+            break;
     }
-    cout << endl;
-    return target;
-}
-
-void Player::add_card(Card* card){
-    card->set_selectable(false);
-    hand.push_back(card);
-}
-
-void Player::reset_AllCards_selectable(){
-    Player* temp = this;
-    do{
-        for(int i = 0; i < temp->hand.size(); ++i){
-            temp->hand[i]->set_selectable(true);
-        }
-        temp = temp->next_player;
-    }while (temp != this);
-}
-
-void Player::print_select_target_list(int& player_index){
-    Player* temp = this->next_player;
-    player_index = 1;
-    cout << "Choose a player to target: " << endl;
-    while (temp != this){
-        cout << player_index << ". " << temp->get_name() << endl;
-        temp = temp->next_player;
-        player_index++;
-    }
+    return true;
 }
 
 void Player::use_card(int index){
@@ -343,117 +374,89 @@ void Player::use_card(int index){
     }
 }
 
-void Player::set_type(const Player::Type& type){
-    this->type = type;
-}
-
-// only for use card select but not the passing/exchanging/drawing/abandoning
-bool Player::can_select_card(int index){
-    if (index>=hand.size()||index<0){
-        return false;
+void Player::print_select_target_list(int& player_index){
+    Player* temp = this->next_player;
+    player_index = 1;
+    cout << "Choose a player to target: " << endl;
+    while (temp != this){
+        cout << player_index << ". " << temp->get_name() << endl;
+        temp = temp->next_player;
+        player_index++;
     }
-    switch (static_cast<int>(hand[index]->get_type())){
-        case 1: 
-            return true;
-        case 2: // cant use CULPRIT unless it is the less card
-            if (hand.size()!=1){
-                return false;
-            }
-            break;
-
-        case 3: // DETECTIVE cant be use when having 4 cards unless holding 1 CULPRIT and 3 DETECTIVE or 4 DETECTIVE
-            if (hand.size() == 4){
-                for(int i = 0; i < hand.size(); ++i){
-                    if (static_cast<int>(hand[i]->get_type()) != 3 && static_cast<int>(hand[i]->get_type()) != 2)
-                        return false;
-                }
-            }
-            break;
-
-        default: // force first finder to use first finder
-            if(has_card(1) != -1)
-                return false;
-            break;
-    }
-    return true;
 }
 
-string Player::get_name() const{
-    return name;
-}
-
-string Player::get_type() const{
-    switch(type){
-        case Player::Type::CIVILIAN:
-            return "Civilian";
-        case Player::Type::CULPRIT:
-            return "Culprit";
-        case Player::Type::ACCOMPLICE:
-            return "Accomplice";
-    }
-    return "";
-}
-
-string Player::get_hand() const{
-    string card_list = "[ ";
-    
-    for (int i = 0; i<hand.size(); i++){
-        switch(static_cast<int>(hand[i]->get_type())){
-            case 1:{
-                card_list += "FIRST_FINDER  ";
-                break;
-            }
-            case 2:{
-                card_list += "CULPRIT  ";
-                break;
-            }
-            case 3:{
-                card_list += "DETECTIVE  ";
-                break;
-            }
-            case 4:{
-                card_list += "ALIBI  ";
-                break;
-            }
-            case 5:{
-                card_list += "ACCOMPLICE  ";
-                break;
-            }
-            case 6:{
-                card_list += "WITNESS  ";
-                break;
-            }
-            case 7:{
-                card_list += "BYSTANDER  ";
-                break;
-            }
-            case 8:{
-                card_list += "BASTET  ";
-                break;
-            }
-            case 9:{
-                card_list += "INFO_EXCHANGE  ";
-                break;
-            }
-            case 10:{
-                card_list += "RUMOR  ";
-                break;
-            }
-            case 11:{
-                card_list += "BARTER  ";
-                break;
-            }
-            case 12:{
-                card_list += "JUVENILE  ";
-                break;
-            }
+Player* Player::select_target(){
+    Player* target = this;
+    int player_index;
+    int target_index;
+    while(true){
+        target = this;
+        print_select_target_list(player_index);
+        cin >> target_index;
+        while (target_index<0 || target_index>player_index){
+            cout << "Out of Bounds." << endl;
+            print_select_target_list(player_index);
+            cin >> target_index;
         }
+
+        for (int i=0; i<target_index; i++){
+            target = target->next_player;
+        }
+        if(target->hand.size() > 0) // if the target has no cards repeat to choose target again 
+            break;
+        cout << "You can't choose players with no cards." << endl << endl;
     }
-    
-    card_list += "]";
-    return card_list;
+    cout << endl;
+    return target;
 }
 
-Card* Player::get_card(const int& index) const{
-    return hand[index];
+int Player::exchange_select_card(){
+    if(hand.size() == 0)
+        return -1;
+    if(hand.size() == 1 && hand[0]->can_use() == false)
+        return -1;
+    int selected_card;
+    cout << get_name() << ": Please select a card to pass/exchange/abandon." << endl;
+    cout << "Card List: " << get_hand() << endl;
+    do{
+        cin >> selected_card;
+        selected_card--;
+        if(hand[selected_card]->can_use() == false){
+            cout << "This card cannot be passed again." << endl;
+        }
+    }while(hand[selected_card]->can_use() == false);
+    return selected_card;
+}
+
+int Player::draw_select_card(){
+    if(prev_player->hand.size() == 0)
+        return -1;
+    if(prev_player->hand.size() == 1 && prev_player->hand[0]->can_use() == false)
+        return -1;
+    int draw_selected_card;
+    cout << get_name() << ": Please select a card you want to draw from " << prev_player->get_name() << endl;
+    cout << "Card List: " << prev_player->get_hand() << endl;
+    do{
+        cin >> draw_selected_card;
+        draw_selected_card--;
+        if(prev_player->hand[draw_selected_card]->can_use() == false){
+            cout << "This card cannot be drawn again." << endl << endl;
+        }
+    }while(prev_player->hand[draw_selected_card]->can_use() == false);
+    return draw_selected_card; 
+}
+
+void Player::add_card(Card* card){
+    card->set_selectable(false);
+    hand.push_back(card);
+}
+
+void Player::reset_AllCards_selectable(){
+    Player* temp = this;
+    do{
+        for(int i = 0; i < temp->hand.size(); ++i){
+            temp->hand[i]->set_selectable(true);
+        }
+        temp = temp->next_player;
+    }while (temp != this);
 }
